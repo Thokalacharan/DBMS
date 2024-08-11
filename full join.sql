1493ea5a651c5ad9a46ddee5b6c9f208efb53f3e
Thokalacharan <166838214+Thokalacharan@users.noreply.github.com>
2024-08-11 23:40:30 +0530

    Create full join.sql

---
+++ full join.sql
@@ -0,0 +1,15 @@
+create table r(a varchar(6),b varchar(6),c number(8));
+create table s(c number(6),d varchar(6));
+insert into r values('a1','b1',10);
+insert into r values('a2','b2',20);
+insert into r values('a3','b3',30);
+insert into r values('a4','b4',40);
+insert into s values(10,'d1');
+insert into s values(15,'d2');
+insert into s values(22,'d3');
+insert into s values(15,'d3');
+insert into s values(25,'d4');
+select *
+from r full join s
+on r.c=s.c;
+
